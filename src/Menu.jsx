21f450cd9f68302f9bd8efe00daf35b59c44ffe3
Adrian Edwards <adrian@adriancedwards.com>
2024-10-03 11:53:35 -0400

    fix build error

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -8,7 +8,7 @@ const Menu = ({ items }) => {
         <a
           key={index}
           href={item.link}
-		  className={`menu-button ${item.enabled == false?  ' disabled' : ''}`}
+		  className={`menu-button ${item.enabled === false ?  ' disabled' : ''}`}
           target="_blank"
           rel="noopener noreferrer"
         >
